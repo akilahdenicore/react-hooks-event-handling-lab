e2af715a60a1ddec5d36ee0149d5f95e00c0687a
Akilah Snowden <akilahdsnowden@gmail.com>
2022-05-22 12:40:37 -0400

    commit tree ee658b3af0d4c50f992135092d7e3f9eccf8b4dd

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+//render button "Eyes on me", event handler listens for focus and blur events
+//focus fire --> console.log 'Good!'
+//blur event fires, console.log "Hey!, Eyes on me!"
+
+function EyesOnMe(){
+    function listenFocus() {
+        console.log('Good!')
+    }
+    function listenBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        //invoke function in JSX syntax
+        <button onFocus={listenFocus} onBlur={listenBlur}>
+        </button>
+    );
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
+
+
+function Keypad() {
+    function renderInput(){
+        console.log('Entering a password...')
+    }
+
+    return (
+        // onChange attribute is useful for handling changes to input values
+    <input type="password" onChange={renderInput}/>
+    )
+}
+
+//export
+
+export default Keypad 
